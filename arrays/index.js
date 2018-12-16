61fc5820a976bf8f877b0de041e1cb4be6267dd0
muhammadali448 <muhammadali19960@outlook.com>
2018-12-16 10:12:17 +0500

    arrays done

---
+++ arrays/index.js
@@ -0,0 +1,64 @@
+// var array_name; //declaration 
+// array_name = [val1,val2,valn..]   //initialization 
+// OR 
+// var array_name = [val1,val2…valn]
+
+// array_name[subscript]
+
+// var alphas; 
+// alphas = ["1","2","3","4"] 
+// console.log(alphas[0]); 
+// console.log(alphas[1]);
+
+// var nums = [1,2,3,3] 
+// console.log(nums[0]); 
+// console.log(nums[1]); 
+// console.log(nums[2]); 
+// console.log(nums[3]);
+
+// var arr_names = new Array(4)  
+// for(var i = 0;i<arr_names.length;i++) { 
+//    arr_names[i] = i * 2 
+//    console.log(arr_names[i]) 
+// }
+
+// ar names = new Array("Mary","Tom","Jack","Jill") 
+// for(var i = 0;i<names.length;i++) { 
+//    console.log(names[i]) 
+// }
+
+// var numbers = [1, 2, 3]; 
+// var oddNumber = numbers.find((x) => x % 2 == 1); 
+// console.log(oddNumber); // 1
+
+// var numbers = [4, 2, 3]; 
+// var oddNumber = numbers.findIndex((x) => x % 2 == 1); 
+// console.log(oddNumber); // 0 
+
+// var numbers = [1, 2, 3]; 
+// var val = numbers.entries(); 
+// console.log(val.next().value);  
+// console.log(val.next().value);  
+// console.log(val.next().value);
+
+// var numbers = [1, 2, 3]; 
+// var val= numbers.entries(); 
+// console.log([...val]);
+
+// "use strict" 
+// for (let i of Array.from('hello')) { 
+//    console.log(i) 
+// }
+
+// console.log(Array.from(['a', 'b'].keys()))
+
+// "use strict" 
+// var nums = [1001,1002,1003,1004] 
+// for(let j in nums) { 
+//    console.log(nums[j]) 
+// }
+
+// var arr = [12,13] 
+// var[x,y] = arr 
+// console.log(x) 
+// console.log(y)
